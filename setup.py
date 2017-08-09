18736045e455f38a67781b4c9fa44b18d0d7620d
Matteo Cerutti <matteo.cerutti@rkrepsilon.com>
2017-08-09 13:04:22 +0100

    adding development dockerfile

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ from fluffy import __version__ as version
 with open(os.path.join(os.getcwd(), 'README.md')) as f:
     readme = f.read()
 
+install_requires = []
+with open(os.path.join(os.getcwd(), 'requirements.txt')) as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name='pyfluffy',
     version=version,
@@ -22,15 +26,7 @@ setup(
         'firewall as a service',
         'iptables as a service'
     ],
-    install_requires=[
-        'flasgger',
-        'Flask',
-        'Flask-API',
-        'subprocess32',
-        'futures',
-        'pyroute2',
-        'python-unshare'
-    ],
+    install_requires=install_requires,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
